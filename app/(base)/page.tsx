3ffe842b6336e5e43884b558a9231657d72f5382
Abhishek Kaith <abhishekkaith76@gmail.com>
2024-06-02 23:12:45 +0530

    home banner

--- app/(base)/page.tsx
+++ app/(base)/page.tsx
@@ -1,9 +1,30 @@
+import { buttonVariants } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
+import { SendHorizontal } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 function page() {
     return (
-        <section className="h-screen relative">
+        <section className="h-[calc(100dvh-var(--header-height))] overflow-hidden">
+            <div className="container h-full flex items-center justify-center flex-col gap-4">
+                <h1 className="text-4xl md:text-6xl font-bold bg-clip-text text-transparent bg-grt">
+                    ZTI Studio
+                </h1>
+                <p className="text-lg md:text-2xl">
+                    Welcome to the Pixel Party!
+                </p>
+                <Link
+                    href={'/contact'}
+                    className={cn(
+                        buttonVariants(),
+                        'flex bg-grt text-base font-medium'
+                    )}
+                >
+                    Get Started
+                    <SendHorizontal className="ml-2" height={18} width={18} />
+                </Link>
+            </div>
             <RedCircle />
             <PurpleCircle />
         </section>
